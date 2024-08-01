7c82b35b88186b9ddd5b96668173bf9a4e7c3e58
LinnEriksson89 <123004529+LinnEriksson89@users.noreply.github.com>
2024-08-01 12:13:45 +0200

    adding account creation

---
+++ src/js/createaccount.js
@@ -0,0 +1,58 @@
+"use strict";
+
+//URL for the API and other variables.
+let url = "http://127.0.0.1:4000/api/register";
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const submitAccount = document.getElementById("submitaccount");
+
+//On load run init.
+window.onload = init();
+
+//Init-function. Adds an eventlistener to submit-button.
+function init() {
+    submitAccount.addEventListener("click", createAccount);
+}
+
+//Use fetch to create account
+function createAccount (event) {
+    event.preventDefault();
+
+    //Variables.
+    let username = usernameInput.value;
+    let password = passwordInput.value;
+    let jsonString = JSON.stringify({
+        username: username,
+        password: password
+    })
+
+    //Fetch with post.
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: jsonString
+    })
+    .then(response => {
+        if(response.status != 201) {
+            //Show error message if account creation fails.
+            let message = document.getElementById("registermessage");
+            message.innerHTML = "Kontot kunde inte skapas!";
+            return;
+        } else {
+            //Show information that account was created.
+            let message = document.getElementById("registermessage");
+            message.innerHTML = "Kontot har skapats. <a href='login.html'>Logga in</a> på inloggningssidan.";
+        }
+        return response.json()
+        .then(clearForm)
+        .catch(err => console.log(err))
+    })
+}
+
+//Clear form.
+function clearForm() {
+    usernameInput.value = "";
+    passwordInput.value = "";
+}
